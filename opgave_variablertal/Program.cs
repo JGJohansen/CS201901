dca9cfdefd89433e15a932ecaea44c3f779557c6
Julian <admin@TA-829-02>
2019-01-14 15:52:37 +0100

    forsøgt at lave opgaver.

--- opgave_variablertal/Program.cs
+++ opgave_variablertal/Program.cs
@@ -10,6 +10,28 @@ namespace opgave_variablertal
     {
         static void Main(string[] args)
         {
+
+            int heltal = 10;
+            heltal++;
+            heltal--;
+            heltal += 10;
+            Console.WriteLine(heltal);
+
+            double kommatal = 12.5;
+            kommatal++;
+            kommatal--;
+            kommatal *= 2;
+            Console.WriteLine(kommatal);
+
+
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
         }
     }
 }
---
+++ opgave_variablertaldel2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave_variablertaldel2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Indtast tal 1");
+            string tal1 = Console.ReadLine();
+            Console.WriteLine("Indtast tal 2");
+            string tal2 = Console.ReadLine();
+
+            
+           
+            double result = System.Convert.ToDouble(tal1) + System.Convert.ToDouble(tal2);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
