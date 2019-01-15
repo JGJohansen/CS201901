b2327c7e4a01907eae04ac84ec5d4b00653e7974
Julian <admin@TA-829-02>
2019-01-15 11:32:45 +0100

    pre lunch save

---
+++ variabler2_datoformatering/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variabler2_datoformatering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+
+            d1 = d1.AddDays(10);
+
+            Console.WriteLine(d1);
+
+            d1 = d1.AddMinutes(20);
+            Console.WriteLine(d1);
+
+            d1 = d1.AddMonths(-1);
+            Console.WriteLine(d1);
+
+
+          
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
