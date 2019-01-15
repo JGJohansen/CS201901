250df43d9e04ba5c9ab472877ce876abfaff259c
Julian <admin@TA-829-02>
2019-01-15 12:22:51 +0100

    ..

--- variabler2_datoformatering/Program.cs
+++ variabler2_datoformatering/Program.cs
@@ -24,6 +24,26 @@ namespace variabler2_datoformatering
             d1 = d1.AddMonths(-1);
             Console.WriteLine(d1);
 
+            DateTime d2 = DateTime.Now;
+
+            DateTime d3 = new DateTime(2019, 01, 01);
+
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+
+            TimeSpan timeDiff = d2.Subtract(d3);
+            Console.WriteLine(timeDiff);
+
+            Console.WriteLine(timeDiff.Days);
+
+            TimeSpan t3 = new TimeSpan(16, 00, 00);
+
+            Console.WriteLine(t3);
+
+            Console.WriteLine();
+
+
+
 
           
             if (System.Diagnostics.Debugger.IsAttached)
