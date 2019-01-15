80dcb7e4d9b816c9a5b2a1d6835908579e8d304d
Julian <admin@TA-829-02>
2019-01-15 14:29:54 +0100

    første opgave i konstanter om enum lavet.

---
+++ Konstanter_structurer_enum/KortKulør.cs
@@ -0,0 +1,10 @@
+﻿namespace Konstanter_structurer_enum
+{
+    enum KortKulør
+    {
+        Spar = 10,
+        Hjerter = 20,
+        Klør = 30,
+        Ruder = 40
+    }
+}
---
+++ Konstanter_structurer_enum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstanter_structurer_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            KortKulør kortKulør1 = KortKulør.Hjerter;
+            int kortVærdi1 = Convert.ToInt32(kortKulør1);
+
+          
+            Console.WriteLine(kortKulør1);
+            Console.WriteLine(kortVærdi1);
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
