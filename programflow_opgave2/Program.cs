b1f08c7f87c58ed4abc942abda6b702c6cff2fbe
Hrværk <Hrvrk@HRVRK-PC>
2019-01-18 22:53:08 +0100

    end of opgave med continue break

---
+++ programflow_opgave2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programflow_opgave2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                
+                if (i % 3 != 0)
+                
+                    continue;
+
+                if (i > 15)
+
+                    break;
+
+                Console.WriteLine(i);
+                
+                
+
+
+            }
+
+
+            for (int q = 0; q < 11; q++)
+            {
+                if (q == 4 || q == 5)
+                    continue;
+                if (q == 8)
+                    break;
+
+                Console.WriteLine(q);
+            }
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
