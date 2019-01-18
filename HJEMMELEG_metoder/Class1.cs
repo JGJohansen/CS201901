5ec6bd0bef6c6cd5779f86d3ae090c419a81a9ec
Hrværk <Hrvrk@HRVRK-PC>
2019-01-18 23:17:05 +0100

    legde lidt med metoder

---
+++ HJEMMELEG_metoder/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HJEMMELEG_metoder
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int test = Metoder.Metode1(4, 2);
+            Console.WriteLine(test);
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+
+        
+    }
+}
---
+++ HJEMMELEG_metoder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HJEMMELEG_metoder
+{
+    public class Metoder
+    {
+        public static int Metode1(int a, int b)
+        {
+
+            if (a > 2)
+             return a * b;
+            else
+            
+                return 2;
+            
+                
+        }
+    }
+}
