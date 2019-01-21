8fedcb64388398153adf1e8a07d0cca44bde5060
julian <admin@TA-828-04>
2019-01-21 10:01:53 +0100

    lavet opgave metoder 1

---
+++ Metoder_opg1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metoder_opg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int res = LægSammen(5, 2);
+            Console.WriteLine(res);
+
+            double areal = BeregnAreal(2.5);
+            Console.WriteLine(areal);
+
+            Udskriv("Dette er en test");
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+
+        static int LægSammen(int a, int b){
+
+            int z = a + b;
+            return z;
+
+        }
+
+        static double BeregnAreal(double radius) {
+            double z = 2 * radius * Math.PI;
+
+            return z;
+        }
+
+        static void Udskriv(string txt) {
+            
+            Console.WriteLine(txt);
+        }
+        
+    }
+}
