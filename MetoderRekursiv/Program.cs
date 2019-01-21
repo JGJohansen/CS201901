219911822003e45c047ae3bf74882ccfd8dff981
julian <admin@TA-828-04>
2019-01-21 12:42:26 +0100

    nyt model. fejl

---
+++ MetoderRekursiv/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetoderRekursiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LøkkeSomMetode(1,50 );
+            
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+
+        static void LøkkeSomMetode(int start, int stop)
+        {
+            Console.WriteLine(start);
+
+            if (start == stop)
+                return;
+            start++;
+            LøkkeSomMetode(start, stop);
+
+           
+        }
+
+
+    }
+
+
+
+}
---
+++ MetoderRekursivAdv/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetoderRekursivAdv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stien = @"C:\Users\admin\Desktop";
+
+            PrintFiler(stien);
+
+            
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }   
+        }
+
+        static void PrintFiler(string sti)
+        {
+            var filer = System.IO.Directory.GetFiles(sti);
+            foreach (string fil in filer) { 
+                Console.WriteLine(fil);
+                System.IO.File.AppendAllText(@"c:\temp\resultat.txt", fil+"\r\n");
+            }
+
+            var mapper = System.IO.Directory.GetDirectories(sti);
+            foreach (string mappe in mapper)
+                PrintFiler(mappe);
+
+
+        }
+    }
+}
