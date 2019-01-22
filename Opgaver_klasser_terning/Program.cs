1dd11ec680b6faea1dc8ff05e7e87e2776945703
julian <admin@TA-828-04>
2019-01-22 10:01:43 +0100

    lavet separat fil for terning

--- Opgaver_klasser_terning/Program.cs
+++ Opgaver_klasser_terning/Program.cs
@@ -21,52 +21,14 @@ namespace Opgaver_klasser_terning
             t2.Ryst();
             t2.Skriv();
 
+            Console.WriteLine(t2.Værdi);
+            Console.WriteLine(t1.Værdi);
+
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 Console.Write("Press any key to continue . . . ");
                 Console.ReadKey();
             }
-        }
-    }
-
-    public class Terning
-    {
-
-        public int Værdi;
-        private bool snyd;
-        private static Random rnd = new Random();
-        public void Skriv()
-        {
-
-            Console.WriteLine("["+this.Værdi+"]");
-            ; 
-        }
-
-        public int Ryst()
-        {
-            if (this.snyd == true)
-            {
-                return this.Værdi = 6;
-            }
-            else
-            {
-                return this.Værdi = rnd.Next(1, 7);
-            }
-            
-
-        }
-
-        public Terning()
-        {
-            this.Værdi = 1;
-            this.snyd = false;
-        }
-
-        public Terning(bool snyd)
-        {
-            this.Værdi = 1;
-            this.snyd = snyd;
-            this.Ryst();
         }
     }
 }
---
+++ Opgaver_klasser_terning/Terning.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Opgaver_klasser_terning
+{
+    public class Terning
+    {
+
+        public int Værdi;
+        private bool snyd;
+        private static Random rnd = new Random();
+        public void Skriv()
+        {
+
+            Console.WriteLine("["+this.Værdi+"]");
+            ; 
+        }
+
+        public int Ryst()
+        {
+            if (this.snyd == true)
+            {
+                return this.Værdi = 6;
+                // husk at tilføje hvad det er den skal returnere til. ikke bare hvad den skal returnere -.-
+            }
+            else
+            {
+                return this.Værdi = rnd.Next(1, 7);
+            }
+            
+
+        }
+
+        public Terning()
+        {
+            this.Værdi = 1;
+            this.snyd = false;
+        }
+
+        public Terning(bool snyd)
+        {
+            this.Værdi = 1;
+            this.snyd = snyd;
+            this.Ryst();
+        }
+    }
+}
